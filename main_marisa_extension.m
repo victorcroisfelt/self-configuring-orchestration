29752da14df288870abfb1a8bafc308fe4f37792
Victor Croisfelt <victorcroisfelt@gmail.com>
2022-07-30 17:02:36 +0200

    Main changes

--- main_marisa_extension.m
+++ main_marisa_extension.m
@@ -48,7 +48,7 @@ Ny = 1;
 % Number of RIS elements
 N = Nx * Ny;
 
-% Nr codebook elements
+% Number codebook elements
 M_cod = 64;
 
 % HRIS absorption parameter
@@ -76,7 +76,7 @@ tau_c = 2 * tau_est;
 % Communication length
 tau_com = tau_est;
 
-%% Simulation parameters
+%% Simulation Parameters
 
 % Range of probing relative lenght
 C_vec = 2.^(1:6);     % iterate
@@ -89,93 +89,105 @@ tau_ref = tau_c - tau_pro;
 
 % Number of setups (simulation runs)
 N_setup = 50;
-N_channel_realizatios = 50;
+N_channel_realizations = 50;
 
 % Don't know what is that
 I = 1;  % risma iterations
 Q_bit = 1;
 
-%% Simulation setup
+%% Simulation Setup
+
+% Parameterized variable to define area of interest
 scenario_size = 50;
 
+% Limits of the area of the interest
 x_lim = [0, scenario_size];
 y_lim = [-scenario_size, scenario_size];
 
 % RISs
 ris_0 = [0;y_lim(1)];                                        % Coordinates of RIS
-ris_0_el = ris_0+lambda./2.*cat(1,0:N-1,zeros(1,N));         % Coordinates of RIS elements
+ris_0_el = ris_0+lambda./2.*cat(1, 0:N-1, zeros(1, N));         % Coordinates of RIS elements
 
+% BS coordinates
 bs = [-scenario_size;0];
 
 % Geometry (downlink)
-phiB_0_a = atan2(bs(2)-ris_0(2),bs(1)-ris_0(1)); %135 deg Angle of Arrival BS-RIS
-phiB_0_d = atan2(ris_0(2)-bs(2),ris_0(1)-bs(1)); %-45 deg Angle of Departure BS-RIS
+phiB_0_a = atan2(bs(2)-ris_0(2), bs(1)-ris_0(1)); %135 deg Angle of Arrival BS-RIS
+phiB_0_d = atan2(ris_0(2)-bs(2), ris_0(1)-bs(1)); %-45 deg Angle of Departure BS-RIS
 d_0_G = norm(bs-ris_0);                          % Distance BS-RIS
 
 % pilots
-pilots = diag(ones(K,1));
+pilots = diag(ones(K, 1));
 
 %% Results
-detected_ue_sig = zeros(numel(C_vec), N_setup, N_channel_realizatios);
-detected_ue_pow = zeros(numel(C_vec), N_setup, N_channel_realizatios);
+detected_ue_sig = zeros(numel(C_vec), N_setup, N_channel_realizations);
+detected_ue_pow = zeros(numel(C_vec), N_setup, N_channel_realizations);
 
-MSE_cha_est_sig = zeros(numel(C_vec), N_setup, N_channel_realizatios);
-MSE_cha_est_pow = zeros(numel(C_vec), N_setup, N_channel_realizatios);
+MSE_cha_est_sig = zeros(numel(C_vec), N_setup, N_channel_realizations);
+MSE_cha_est_pow = zeros(numel(C_vec), N_setup, N_channel_realizations);
 
 %% Simulation
+
 rng(49)
+
 for ind_setup = 1:N_setup
     
-    ue_s = [(x_lim(2)-x_lim(1))*rand(1,K,N_setup)+x_lim(1); (y_lim(2)-y_lim(1))*rand(1,K,N_setup)+y_lim(1)]; % UEs coordinates --- drawn in x between [x_lim(1), x_lim(2)], y between [y_lim(1), y_lim(2)]
+    ue_s = [(x_lim(2)-x_lim(1))*rand(1, K, N_setup) + x_lim(1); (y_lim(2)-y_lim(1))*rand(1, K, N_setup)+y_lim(1)]; % UEs coordinates --- drawn in x between [x_lim(1), x_lim(2)], y between [y_lim(1), y_lim(2)]
     
     %ue = [(x_lim(2)-x_lim(1))*rand(1,K)+x_lim(1); (y_lim(2)-y_lim(1))*rand(1,K)+y_lim(1)];
-    ue = ue_s(:,1:K,ind_setup);
+    ue = ue_s(:, 1:K, ind_setup);
     
     % Compute geometry wrt BS
     rot_angle = 90/180*pi;
     [bs_rot, ue_rot, ~] = rotate_geometry(rot_angle, bs, ue, ris_0_el, false());
     
     % Compute UE angles wrt BS and RIS
-    phiUB = atan2(ue_rot(2,:)-bs_rot(2),ue_rot(1,:)-bs_rot(1))';
-    phiU_0 = atan2(ue(2,:)-ris_0(2),ue(1,:)-ris_0(1))';
+    phiUB = atan2(ue_rot(2, :) - bs_rot(2), ue_rot(1, :)-bs_rot(1))';
+    phiU_0 = atan2(ue(2, :)-ris_0(2), ue(1, :)-ris_0(1))';
     
     % Compute UE distances wrt BS and RIS
-    d_Bu = zeros(K,1);
-    d_u_0 = zeros(K,1);
+    d_Bu = zeros(K, 1);
+    d_u_0 = zeros(K, 1);
     
     for k=1:K
-        d_Bu(k) = norm(bs-ue(:,k));                                 % Distance BS-UE
-        d_u_0(k) = norm(ris_0-ue(:,k));                             % Distance RIS-UE
+
+        % Distance BS-UE
+        d_Bu(k) = norm(bs - ue(:, k));   
+
+        % Distance RIS-UE
+        d_u_0(k) = norm(ris_0-ue(:, k));   
+
     end
     
-    
-    for ind_ch =1:N_channel_realizatios
-        %% compute blockage probability of paths
-        [block_u_0] = blockage_path(d_u_0, lambda_b, radius_B, height_B, height_U, hheight_A); % ris block
-        [block_Bu] = blockage_path(d_Bu, lambda_b, radius_B, height_B, height_U, hheight_A);    % bs  block
+    for ind_ch =1:N_channel_realizations
         
-        %% Channels
-        [a_R_los,~,G_los,h_los,h_D_los,a_BSU_los,a_BSR_los] = compute_channels(d_0_G,d_u_0,d_Bu,[beta,beta_nlos],N,M,phiB_0_a,phiB_0_d,phiU_0,phiUB,el_dist, block_u_0, block_Bu);
+        % Compute blockage probability of paths
+        [block_u_0] = blockage_path(d_u_0, lambda_b, radius_B, height_B, height_U, hheight_A); % RIS block
+        [block_Bu] = blockage_path(d_Bu, lambda_b, radius_B, height_B, height_U, hheight_A); % BS block
         
-        %channel realization for
-        h_los = h_los.*exp(1i.*rand(1,K).*2*pi);
-        h_D_los = h_D_los.*exp(1i.*rand(1,K).*2*pi);
+        % Channels
+        [a_R_los, ~, G_los, h_los, h_D_los, a_BSU_los, a_BSR_los] = compute_channels(d_0_G, d_u_0, d_Bu, [beta,beta_nlos], N, M, phiB_0_a, phiB_0_d, phiU_0, phiUB,el_dist, block_u_0, block_Bu);
         
-        
+        % Channel realizations
+        %h_los = h_los .* exp(1i.*rand(1, K).*2*pi);
+        %h_D_los = h_D_los .* exp(1i.*rand(1, K).*2*pi);
+       
         for ind_d = 1:length(C_vec)
+
             D = C_vec(ind_d);
             C = D;
             
-            message = ['run', num2str(ind_setup), '/',num2str(N_setup),' Chan',num2str(ind_ch),'/',num2str(N_channel_realizatios),' C',num2str(ind_d),'/',num2str(length(C_vec))];
+            message = ['run', num2str(ind_setup), '/',num2str(N_setup),' Chan',num2str(ind_ch),'/',num2str(N_channel_realizations),' C',num2str(ind_d),'/',num2str(length(C_vec))];
             disp(message)
             
-            [~,theta_in, phi_vec] = generate_codebook(C,N);
+            % Generate configuration codebook according to Eq. (12)
+            [~, theta_in, phi_vec] = generate_codebook(C, N);
             
             % Probing codebook definition
-            Theta_prob_sig = repmat(eye(N), [1,1,C]);
-            Theta_prob_pow = zeros(N,N,C);
+            Theta_prob_sig = repmat(eye(N), [1, 1, C]);
+            Theta_prob_pow = zeros(N, N, C);
             for t = 1:C
-                Theta_prob_pow(:,:,t) = diag(theta_in(:,t));
+                Theta_prob_pow(:, :, t) = diag(theta_in(:, t));
             end
             
             % Received signal at RIS
@@ -187,7 +199,7 @@ for ind_setup = 1:N_setup
             [Theta_opt_pow, det_rate_pow] = MARISA_EXTENSION(Y_r_pow, theta_in, false_alarm_prob, phiB_0_a, sigma2n, 'power');
             
             % Equivalent BS-UE channel
-            Theta_over_blocks = cat(3, Theta_prob_pow,Theta_prob_sig); % to change after hris optimization
+            Theta_over_blocks = cat(3, Theta_prob_pow, Theta_prob_sig); % to change after hris optimization
             [H_circ] = equivalent_BS_UE_channel(Theta_over_blocks, h_los, G_los, h_D_los, eta);
             [Y_b]    = received_signal_BS(H_circ, pilots, P_ue, sigma2n);
             
@@ -195,7 +207,8 @@ for ind_setup = 1:N_setup
             [MSE_sig] = channel_estiamation_MSE(M,L,K,sigma2n,P_ue,G_los,Theta_prob_sig, Theta_opt_sig, h_los, eta);
             [MSE_pow] = channel_estiamation_MSE(M,L,K,sigma2n,P_ue,G_los,Theta_prob_pow, Theta_opt_pow, h_los, eta);
 
-            
+   
+            % Save simulation results
             detected_ue_sig(ind_d,ind_setup,ind_ch) = det_rate_sig;
             detected_ue_pow(ind_d,ind_setup,ind_ch) = det_rate_pow;
             
