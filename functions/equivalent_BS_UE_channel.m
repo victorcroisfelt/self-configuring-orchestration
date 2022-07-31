f4ab309ab532139e52677f89cdfc217a04ffc9bc
Francesco Devoti <francesco.devoti@neclab.eu>
2022-07-31 15:50:54 +0200

    minor changes

--- functions/equivalent_BS_UE_channel.m
+++ functions/equivalent_BS_UE_channel.m
@@ -4,14 +4,7 @@ M = size(G,2);      % number of BS elements
 K = size(H_D,2);    % number of users
 
 H_circ = zeros(M,K,L);
-
-%H_circ_norm = zeros(K,L);
-
 for l = 1:L
-    
     H_circ(:,:,l) = H_D + sqrt(eta).*G'*Theta(:,:,l)'*H;
-    %for k =1:K
-    %    H_circ_norm(k,l) = H_circ(:,k,l)'*H_circ(:,k,l);
-    %end
 end
 end
--- functions/received_signal_BS.m
+++ functions/received_signal_BS.m
@@ -1,4 +1,4 @@
-function [Y] = received_signal_BS(H_circ, pilot, rho, sigma2n)
+function [Y,N] = received_signal_BS(H_circ, pilot, rho, sigma2n)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 K = size(pilot,1);  % nr users
@@ -10,7 +10,10 @@ Y = zeros(M,K,L);
 for l = 1:L
     Y(:,:,l) = H_circ(:,:,l)*pilot;
 end
-Y = Y * sqrt(rho);
-Y = Y + (randn(M,K,L) + 1i*randn(M,K,L))*sqrt(sigma2n/2);
+Y = Y *sqrt(rho);  % correlation function with the pilots
+N = (randn(M,K,L) + 1i*randn(M,K,L))*sqrt(sigma2n/2);
+Y = Y + N;
+
+
 end
 
