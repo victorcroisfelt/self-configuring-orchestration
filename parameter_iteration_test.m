886de384816c8b3a46d9a55a7173130f93c4d779
Francesco Devoti <francesco.devoti@neclab.eu>
2022-07-31 17:06:49 +0200

    double check math

---
+++ parameter_iteration_test.m
@@ -0,0 +1,25 @@
+close all
+clear all
+
+addpath('functions')
+
+plot_flag = false();
+
+M_vec = [32, 64];
+N_vec = [16, 32];
+K_vec = [8, 16];
+L_vec = [64, 128];
+scenario_size_vec = [50,100];
+
+for M = M_vec
+    for Nx = N_vec
+        for K = K_vec
+            for L = L_vec
+                for scenario_size = scenario_size_vec
+                    test_francesco
+                end
+            end
+        end
+    end
+end
+
